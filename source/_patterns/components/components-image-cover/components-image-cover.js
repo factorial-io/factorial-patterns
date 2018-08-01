d13674738b22f860d3d4c38545948f5fdf9e4a87
Milan Matull <milan@factorial.io>
2018-08-01 16:33:17 +0200

    #3 Add contain and cover cases

---
+++ source/_patterns/components/components-image-cover/components-image-cover.js
@@ -0,0 +1,8 @@
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.lazyClass = 'is-lazy';
+window.lazySizesConfig.loadingClass = 'is-loading';
+window.lazySizesConfig.loadedClass = 'is-loaded';
+
+import 'picturefill';
+import 'lazysizes';
+import 'lazysizes/plugins/bgset/ls.bgset';
