23e2403bf2e31cb1b00aef087f1626d51de544ea
Milan Matull <milan@factorial.io>
2019-03-08 17:33:45 +0100

    Autofix code style

--- source/_patterns/components/components-image-cover/components-image-cover.js
+++ source/_patterns/components/components-image-cover/components-image-cover.js
@@ -1,8 +1,8 @@
-window.lazySizesConfig = window.lazySizesConfig || {};
-window.lazySizesConfig.lazyClass = 'is-lazy';
-window.lazySizesConfig.loadingClass = 'is-loading';
-window.lazySizesConfig.loadedClass = 'is-loaded';
+import "picturefill";
+import "lazysizes";
+import "lazysizes/plugins/bgset/ls.bgset";
 
-import 'picturefill';
-import 'lazysizes';
-import 'lazysizes/plugins/bgset/ls.bgset';
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.lazyClass = "is-lazy";
+window.lazySizesConfig.loadingClass = "is-loading";
+window.lazySizesConfig.loadedClass = "is-loaded";
--- source/index.js
+++ source/index.js
@@ -1,4 +1,4 @@
 import "./index.css";
-import "./_patterns/components/components-image/components-image.js";
-import "./_patterns/components/components-image-cover/components-image-cover.js";
-import "./_patterns/components/components-image-contain/components-image-contain.js";
+import "./_patterns/components/components-image/components-image";
+import "./_patterns/components/components-image-cover/components-image-cover";
+import "./_patterns/components/components-image-contain/components-image-contain";
