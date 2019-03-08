1eb979f1cd46f7c5b0091de94a018152b28719e9
Milan Matull <milan@factorial.io>
2019-03-08 17:23:05 +0100

    Install new frontend stack

---
+++ .neutrinorc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  use: [
+    "@factorial/frontend-stack-core"
+  ],
+  options: {
+    source: "source",
+    output: "public",
+    mains: {
+      index: "index.js"
+    }
+  }
+};
--- source/index.js
+++ source/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import "./_patterns/components/components-image/components-image.js";
 import "./_patterns/components/components-image-cover/components-image-cover.js";
 import "./_patterns/components/components-image-contain/components-image-contain.js";
